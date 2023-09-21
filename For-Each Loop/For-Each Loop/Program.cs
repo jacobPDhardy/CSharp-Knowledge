b05926ed0f3ce236a9da5c1291029fb535b9d0ee
jacobPDhardy <145456509+jacobPDhardy@users.noreply.github.com>
2023-09-21 11:25:08 +0100

    For-Each Loop
    For-Each Loop notes

---
+++ For-Each Loop/For-Each Loop/Program.cs
@@ -0,0 +1,7 @@
+﻿
+string alphabetString = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+string[] alphabet = alphabetString.Split(", "); //Creates a list where each index is a letter of the alphabet
+
+foreach (String letter in alphabet){ //Iterates over the list assigning the index currently in use to the variable "letter"
+    Console.WriteLine(letter);
+}
