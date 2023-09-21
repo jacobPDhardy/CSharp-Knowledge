6f890ad6d88c31aabb546e96ce54ebb1959ef3da
jacobPDhardy <145456509+jacobPDhardy@users.noreply.github.com>
2023-09-21 15:44:22 +0100

    Bubble
    Added bubble sort

---
+++ bubble/bubble/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+int[] generateNumbers(int length) {
+
+    int[] numbers = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        numbers[i] = new Random().Next(0, 100000);
+    }
+    return numbers;
+}
+
+void outputNumbers(int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.WriteLine(number);
+    }
+}
+
+int[] bSort(int[] numbers)
+{
+    while (true)
+    {
+        int swaps = 0;
+        for (int i=0;i<numbers.Length-1;i++)
+        {
+                if (numbers[i] > numbers[i+1])
+                {
+                int temp = numbers[i];
+                numbers[i] = numbers[i+1];
+                numbers[i + 1] = temp;
+                swaps++;
+                }
+        }
+        if (swaps == 0)
+            {
+                break;
+            }
+        
+    }
+    return numbers;
+}
+
+int[] numbers = generateNumbers(10000);
+
+var timer = new Stopwatch();
+timer.Start();
+outputNumbers(bSort(generateNumbers(10000)));
+timer.Stop();
+Console.WriteLine(timer.Elapsed.ToString());
