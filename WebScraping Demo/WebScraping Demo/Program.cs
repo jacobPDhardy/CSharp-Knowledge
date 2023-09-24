a9dd89180ea118f9bf7d9dca9af685c35e5437b5
jacobPDhardy <145456509+jacobPDhardy@users.noreply.github.com>
2023-09-24 16:22:25 +0100

    Deleted broken program
    ^^

--- WebScraping Demo/WebScraping Demo/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using HtmlAgilityPack;
-
-string url = "https://en.wikipedia.org/wiki/List_of_SpongeBob_SquarePants_episodes";
-
-var web = new HtmlWeb();
-
-var document = web.Load(url);
-
-Console.WriteLine(document.ToString); 
